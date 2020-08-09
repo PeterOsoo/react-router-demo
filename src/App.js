dbc914e66fb1c8bcae60cc66394aa4fc01c7b162
Peter <Posoo617@gmail.com>
2020-08-09 11:34:31 +0300

    Add Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from "react"
 import "./App.css"
+import { BrowserRouter as Router } from "react-router-dom"
+import Route from "react-router-dom/Route"
 
-function App() {
+const User = ({ match }) => {
+	return <h1>Welcome User {match.params.username} </h1>
+}
+
+const App = () => {
 	return (
-		<div>
-			<h1 className="App">React Router Demo</h1>
-			<p>Route, Link, NavLink, Redirect, Prompt</p>
-		</div>
+		<Router>
+			<div className="App">
+				<Route
+					path="/"
+					exact
+					strict
+					render={() => {
+						return <h1> Welcome home</h1>
+					}}
+				/>
+
+				<Route
+					path="/about/"
+					exact
+					strict
+					render={() => {
+						return <h1> About blog</h1>
+					}}
+				/>
+
+				<Route path="/user/:username" exact strict component={User} />
+			</div>
+		</Router>
 	)
 }
 
