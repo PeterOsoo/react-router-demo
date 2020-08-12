57904a4ddf983f007fd7150aaabd2487b05de332
Peter <Posoo617@gmail.com>
2020-08-12 17:33:47 +0300

    Prompt

--- src/App.js
+++ src/App.js
@@ -1,89 +1,109 @@
-import React, { useState } from "react"
+import React, { Component } from "react"
 import "./App.css"
 import {
 	BrowserRouter as Router,
 	Link,
 	NavLink,
 	Redirect,
+	Prompt,
 } from "react-router-dom"
 import Route from "react-router-dom/Route"
 
-const User = ({ params }) => {
-	return <h1>Welcome User {params.username} </h1>
+const User = params => {
+	return <h1> Welcome User {params.username} </h1>
 }
 
-const App = () => {
-	const [loggedIn, setLoggedIn] = useState(false)
-	return (
-		<Router>
-			<div className="App">
-				<ul>
-					<li>
-						<NavLink to="/" exact activeStyle={{ color: "green" }}>
-							{" "}
-							Home
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/about" exact activeStyle={{ color: "green" }}>
-							{" "}
-							About
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/user/peter" exact activeStyle={{ color: "green" }}>
-							{" "}
-							User Peter
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/user/john" exact activeStyle={{ color: "green" }}>
-							{" "}
-							User John
-						</NavLink>
-					</li>
-				</ul>
+class App extends Component {
+	state = {
+		loggedIn: false,
+	}
+	loginHandle = () => {
+		this.setState(prevState => ({
+			loggedIn: !prevState.loggedIn,
+		}))
+	}
 
-				{/* button  */}
-				<input
-					type="button"
-					value={loggedIn ? "logout" : "login"}
-					onClick={() => setLoggedIn(!false)}
-				/>
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<ul>
+						<li>
+							<NavLink to="/" exact activeStyle={{ color: "green" }}>
+								{" "}
+								Home
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/about" exact activeStyle={{ color: "green" }}>
+								{" "}
+								About
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/user/peter" exact activeStyle={{ color: "green" }}>
+								{" "}
+								User Peter
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/user/john" exact activeStyle={{ color: "green" }}>
+								{" "}
+								User John
+							</NavLink>
+						</li>
+					</ul>
 
-				<Route
-					path="/"
-					exact
-					strict
-					render={() => {
-						return <h1> Welcome home</h1>
-					}}
-				/>
+					<Prompt
+						when={!this.state.loggedIn}
+						message={location => {
+							return location.pathname.startsWith("/user")
+								? "Are you sure?"
+								: true
+						}}
+					/>
+					{/* button  */}
 
-				<Route
-					path="/about"
-					exact
-					strict
-					render={() => {
-						return <h1> About blog</h1>
-					}}
-				/>
+					<input
+						type="button"
+						value={this.state.loggedIn ? "log out" : "log in"}
+						onClick={this.loginHandle.bind(this)}
+					/>
 
-				<Route
-					path="/user/:username"
-					exact
-					strict
-					render={({ match }) =>
-						loggedIn ? (
-							<User username={match.params.username} />
-						) : (
-							<Redirect to="/" />
-						)
-					}
-				/>
-			</div>
-		</Router>
-	)
+					<Route
+						path="/"
+						exact
+						strict
+						render={() => {
+							return <h1> Welcome home</h1>
+						}}
+					/>
+
+					<Route
+						path="/about"
+						exact
+						strict
+						render={() => {
+							return <h1> About blog</h1>
+						}}
+					/>
+
+					<Route
+						path="/user/:username"
+						exact
+						strict
+						render={({ match }) =>
+							this.state.loggedIn ? (
+								<User username={match.params.username} />
+							) : (
+								<Redirect to="/" />
+							)
+						}
+					/>
+				</div>
+			</Router>
+		)
+	}
 }
 
 export default App
